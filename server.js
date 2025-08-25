d1db393a3131cba0359719ec29ce965cd6e57099
imranjaleel <imranjaleel2331@gmail.com>
2025-08-25 12:46:49 +0530

    few changes in server.jj

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 const express = require("express");
-const { createServer } = require("node:http");
 const cors = require("cors");
 const connectToDB = require("./Configs/mongodb.config");
 const UserRouter = require("./Routes/user.routes");
 const TransactionRouter = require("./Routes/transaction.routes");
 const BudgetRouter = require("./Routes/budget.routes");
 const goalRouter = require("./Routes/goal.routes");
-const { Server } = require("socket.io");
 const NotificationRouter = require("./Routes/notification.routes");
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-  cors: { origin: "*", methods: ["GET", "POST"] },
-});
+
 
 const PORT = 3000;
 app.use(cors());
@@ -31,31 +26,10 @@ app.use("/budget", BudgetRouter);
 app.use("/goal", goalRouter);
 app.use("/notification",NotificationRouter)
 
-// store connected users
-let onlineUsers = {};
 
-io.on("connection", (socket) => {
-  console.log("⚡ User connected:", socket.id);
 
-  // when a user logs in, register their socket
-  socket.on("registerUser", (userId) => {
-    onlineUsers[userId] = socket.id;
-    console.log("Registered:", onlineUsers);
-  });
-
-  socket.on("disconnect", () => {
-    for (let uid in onlineUsers) {
-      if (onlineUsers[uid] === socket.id) {
-        delete onlineUsers[uid];
-        break;
-      }
-    }
-    console.log("❌ User disconnected", socket.id);
-  });
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
-module.exports = { server, io, onlineUsers };
+
