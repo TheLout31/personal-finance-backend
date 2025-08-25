d5fc1b6e5e56d8707eabfd2dc2524b77ad551778
imranjaleel <imranjaleel2331@gmail.com>
2025-08-25 13:48:58 +0530

    made few changes

--- server.js
+++ server.js
@@ -28,8 +28,10 @@ app.use("/notification",NotificationRouter)
 
 
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+module.exports = app
 
 
